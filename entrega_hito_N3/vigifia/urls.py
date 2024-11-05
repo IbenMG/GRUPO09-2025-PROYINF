e4ca7b31cace2ad8213e021af7d9662772f2dab9
ClaudioJimenezA <130109134+ClaudioJimenezA@users.noreply.github.com>
2024-11-04 23:09:31 -0300

    cambio en urls

--- entrega_hito_N3/vigifia/urls.py
+++ entrega_hito_N3/vigifia/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('buscador_fuentes.urls')),
+    path('fuentes/', include('buscador_fuentes.urls')),
     path('pdfs/', include('gestor_pdfs.urls')),
 ]
 if settings.DEBUG:
