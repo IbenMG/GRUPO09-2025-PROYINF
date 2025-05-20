1bacdb0b526ccda6a8b654df742fe188aeceecff
josebeltran1 <166769489+josebeltran1@users.noreply.github.com>
2025-05-20 19:59:49 -0400

    Update models.py

--- entrega_hito_N4/gestor_pdfs/models.py
+++ entrega_hito_N4/gestor_pdfs/models.py
@@ -3,8 +3,9 @@ from django.db import models
 class DocumentoPDF(models.Model):
     nombre = models.CharField(max_length=255)
     archivo = models.FileField(upload_to='pdfs/')
-    etiquetas = models.CharField(max_length=255, blank=True)  # Opcional: para almacenar etiquetas en texto
-
+    etiquetas = models.CharField(max_length=255, blank=True)
+    publicado = models.BooleanField(default=False)  # Indica si estÃ¡ publicado/finalizado
+    fecha_publicacion = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
-      return self.nombre
+        return self.nombre
